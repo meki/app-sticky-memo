eb5420de1c1328d966eec93e7faca9a0113e9637
meki <sashidapc373443@gmail.com>
2025-06-28 23:25:19 +0900

    add flet package

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from app-sticky-memo!")
+import flet as ft
 
+def main(page: ft.Page):
+    page.title = "Flet counter example"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-if __name__ == "__main__":
-    main()
+    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+    def minus_click(e):
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+
+    def plus_click(e):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+
+    page.add(
+        ft.Row(
+            [
+                ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
+                txt_number,
+                ft.IconButton(ft.Icons.ADD, on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
+
+ft.app(main)
